636e91f86da70bb4f84d3ba3e0db55052553e327
Wonseon <onesun814@naver.com>
2020-01-07 14:38:25 +0900

    main () 수정

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int add(int,int);
-int sub(int,int);
-int div(int,int);
-int multi(int,int);
+int add(int x,int y);
+int sub(int x,int y);
+int div(int x,int y);
+int multi(int x,int y);
 
-int main(int argc,char* argv[])
+int main()
 {
 
 	int a,b,c;
 	char operator;
 
-	printf("please enter a expression");
+	printf("please enter a expression /n");
 	scanf("%d %c %d",&a,&operator,&b);
 
 	switch(operator)
@@ -25,17 +25,19 @@ int main(int argc,char* argv[])
 		case '*':
 				c= multi(a,b);
 				break;
-	}				
-int add(int x,int y){
+	}
+}
+int add(int x,int y)
+{
 	return x+y;
 	
 }
 
-int div(int x,int y){
+int div(int x,int y)
+{
 	return x/y;
 }
 
-}
 int sub(int x, int y)
 {
 	return x-y;
