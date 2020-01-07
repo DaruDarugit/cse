0d10e32b9541bf77354aea3c3dc7dda91361f429
Wonseon <onesun814@naver.com>
2020-01-07 14:22:40 +0900

    fixed#1 뼈대 작성

--- main.c
+++ main.c
@@ -5,11 +5,26 @@ int sub(int,int);
 int div(int,int);
 int multi(int,int);
 
-int main(int argc,char* argv[]
-){
+int main(int argc,char* argv[])
+{
 
 	int a,b,c;
 	char operator;
-	
+
+	printf("please enter a expression");
+	scanf("%d %c %d",&a,&operator,&b);
+
+	switch(operator)
+	{
+		case '+':
+				c= add(a,b);
+				break;
+		case '-':
+				c= sub(a,b);
+				break;
+		case '*':
+				c= multi(a,b);
+				break;
+	}				
 
 }
